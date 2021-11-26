2f79dd2dbc262a419f7e15c54b6079a999cdbfd3
asmatar <deruelle.arthur@gmail.com>
2021-11-27 00:20:48 +0100

    icon

--- pages/search/[id].js
+++ pages/search/[id].js
@@ -1,3 +1,4 @@
+import { StarIcon } from '@heroicons/react/solid';
 import { useRouter } from 'next/dist/client/router';
 import React, { useState } from 'react';
 import Footer from '../../components/Footer';
@@ -8,10 +9,7 @@ const Room = (accommodationDetail) => {
     console.log('dans one room',accommodationDetail.accommodationDetail.searchResult)
     const [room] = useState(accommodationDetail.accommodationDetail.searchResult)
     const [review] = useState(accommodationDetail.review.searchResult)
-    // console.log(review)
-    // console.log(room)
-    // const [reviewss] = useState(review)
-    // console.log('dans reviewAcco',reviewss)
+ 
         // to get the information from the url we use the router.query
         const router = useRouter()
     const {Tot, formatedStartDate, formatedEndDate, numberOfGuest} = router.query
@@ -20,19 +18,29 @@ console.log('dans id formatedStartDate:', formatedStartDate)
     return (
         <>
         <Header />
+    
         <div className='max-w-7xl mx-auto p-x-8 sm:px-16'>
         {/* titre review star share */}
         <div className='pt-6'>
              <h2 className='text-3xl font-semibold pb-5'>{room.title}</h2>
              <div className='flex justify-between'> 
                 <div className='flex pb-6 text-gray-600'>
-                    <p className='text-black mr-1'>{room.star} </p><span className="underline">({review.length} reviews)</span> 
+                    <div className='flex items-center'>
+                        <StarIcon className='h-5 text-red-400' /><p className='text-black mr-1'>{room.star} </p>
+                        </div>
+                        <span className="underline">({review.length} reviews)</span> 
                     <p className='mx-6'>Superhost</p>
                     <p className='underline'>{room.location}</p>
                 </div>
-                <div>
-                    <a href="" className='mr-8 underline'>Share</a>
-                    <a href="" className='underline'>Save</a>
+                <div className='flex'>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                </svg>
+                    <a href="" className='mr-4 ml-1 underline'>Share</a>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                    <a href="" className='underline ml-1'>Save</a>
                 </div>
              </div>
         </div>
@@ -58,14 +66,32 @@ console.log('dans id formatedStartDate:', formatedStartDate)
             </div>
             <div>
                 <div className='my-6 border-b border-solid border-gray-300'>
-                    <h5 className='text-lg font-semibold '>Enhanced Clean</h5>
+
+                    <div className="flex">
+                    <img src="/images/netoyage.png" className='w-8 mr-2 h-8'/>  
+                    <div>
+                        <h5 className='text-lg font-semibold '>Enhanced Clean</h5>
                     <p className='text-gray-600 mb-4'>This Host committed to Airbnb's 5-step enhanced cleaning process. Show more</p>
-            
-                    <h5 className='text-lg font-semibold'>Self check-in</h5>
-                    <p className='text-gray-600 mb-4'>Check yourself in with the lockbox.</p>
-                
-                    <h5 className='text-lg font-semibold'>Great check-in experience</h5>
+                    </div>
+                    </div>
+                    
+                    <div className="flex">
+                        <img src="/images/porte.jpg" className='w-8 mr-2 h-8'/>  
+                        <div>
+                             <h5 className='text-lg font-semibold'>Self check-in</h5>
+                            <p className='text-gray-600 mb-4'>Check yourself in with the lockbox.</p>
+                        </div>
+                    </div>
+                    
+                   <div className="flex">
+                   <img src="/images/check.png" className='w-8 mr-2 h-8'/>  
+                       <div>
+                           <h5 className='text-lg font-semibold'>Great check-in experience</h5>
                     <p className='text-gray-600 mb-8'>100% of recent guests gave the check-in process a 5-star rating.</p>
+                       </div>
+                   </div>
+                
+                    
                 
                 </div>
             </div>
@@ -76,15 +102,36 @@ console.log('dans id formatedStartDate:', formatedStartDate)
             <h2 className='text-2xl font-semibold mt-6' >what the place offer</h2>
             <div className='flex w-full pb-6'>
                 <div className="flex flex-col w-full leading-9">
-                    <p className=''>Kitchen</p>
-                    <p className=''>Wifi</p>
-                    <p className=''>TV with standard cable</p> 
-                    <p className= ''>Hair dryer</p>
+                    <div className="flex my-4">
+                        <img src="/images/cuisine.png" alt="" className='w-8 mr-4'/> 
+                        <p> Kitchen</p>
+                    </div>
+                    <div className="flex mb-4">
+                        <img src="/images/wifi.png" className='w-8 mr-4 '/>  
+                        <p >Wifi</p>
+                    </div>
+                    <div className="flex mb-4">
+                        <img src="/images/TV.png" className='w-8 mr-4'/>
+                        <p >cable</p> 
+                    </div>
+                    <div className="flex">
+                        <img src="/images/seche.png" className='w-8 mr-4'/>  
+                        <p >Hair dryer</p>
+                    </div>
                 </div>
                 <div className="flex flex-col w-full leading-9"> 
+                    <div className="flex mb-4"> 
+                    <img src="/images/smoke.png" className='w-8 mr-4'/>  
                     <p className=''>Smoking allowed</p>
+                    </div>
+                    <div className="flex mb-4"> 
+                    <img src="/images/work.png" className='w-8 mr-4'/>  
                     <p className=''>Dedicated workspace</p>
+                    </div>
+                    <div className="flex mb-4"> 
+                    <img src="/images/voiture.jpg" className='w-8 mr-4'/>  
                     <p className=''>Paid parking off premises</p>
+                    </div>
                 </div>
             </div>
             </div>
@@ -93,7 +140,13 @@ console.log('dans id formatedStartDate:', formatedStartDate)
                 <div className='border border-solid border-gray-300 rounded-lg p-8'>
                     <div className='flex justify-between mb-6'>
                         <div> <span className='font-medium text-xl'>£{room.price}</span> / night </div>
-                        <div><span className='font-medium'>{room.star}</span> <span className='text-gray-600 underline font-medium'> ({review.length} reviews)</span></div>
+                      
+                            <div className='flex items-center'>
+                            <StarIcon className='h-5 text-red-400' />
+                            <span className='font-medium'>
+                            {room.star}</span> 
+                            <span className='text-gray-600 underline font-medium'> ({review.length} reviews)</span></div>
+                          
                     </div>
                     <div className=' border border-solid border-gray-300 rounded-lg text-sm'>
                         <div className='flex justify-between p-2'>
@@ -122,8 +175,14 @@ console.log('dans id formatedStartDate:', formatedStartDate)
             </div>
         </div>
         {/* review */}
+        <div className="flex mb-4 items-center">
+            <StarIcon className='h-5 text-red-400' />
+            <span className='mx-1 text-xl font-semibold'>{room.star} -</span>
+            
+            <span className='ml-1 text-xl font-semibold'>{review.length} reviews</span>
+        </div>
+        
         <div className='w-full h-full grid grid-cols-2 gap-x-24 border-b border-solid border-gray-300 '>
-
             {
                 review.map((review)=>(
                 <div className='h-40'>
@@ -133,36 +192,7 @@ console.log('dans id formatedStartDate:', formatedStartDate)
                 </div>
                 ))
             }
-            {/* <div className='h-40'>
-                <h5 className='font-semibold w-full'>Ilias</h5>
-                <p className='text-gray-500 mb-4'>August 2021</p>
-                <p className>This is the third time I stay at Lateef’s and every time it’s a wonderful experience. The flat is sparkling clean, very spacious and Lateef is so nice and helpful. 10/10 will always recommend.</p>
-            </div>
-            <div className=' h-40 '>
-                <h5 className='font-semibold w-full'>Ilias</h5>
-                <p className='text-gray-500 mb-4'>August 2021</p>
-                <p className>This is the third time I stay at Lateef’s and every time it’s a wonderful experience. The flat is sparkling clean, very spacious and Lateef is so nice and helpful. 10/10 will always recommend.</p>
-            </div>
-            <div className=' h-40 '>
-                <h5 className='font-semibold w-full'>Ilias</h5>
-                <p className='text-gray-500 mb-4'>August 2021</p>
-                <p className>This is the third time I stay at Lateef’s and every time it’s a wonderful experience. The flat is sparkling clean, very spacious and Lateef is so nice and helpful. 10/10 will always recommend.</p>
-            </div>
-            <div className='h-40  '>
-                <h5 className='font-semibold w-full'>Ilias</h5>
-                <p className='text-gray-500 mb-4'>August 2021</p>
-                <p className>This is the third time I stay at Lateef’s and every time it’s a wonderful experience. The flat is sparkling clean, very spacious and Lateef is so nice and helpful. 10/10 will always recommend.</p>
-            </div>
-            <div className=' h-40 '>
-                <h5 className='font-semibold w-full'>Ilias</h5>
-                <p className='text-gray-500 mb-4'>August 2021</p>
-                <p className>This is the third time I stay at Lateef’s and every time it’s a wonderful experience. The flat is sparkling clean, very spacious and Lateef is so nice and helpful. 10/10 will always recommend.</p>
-            </div>
-            <div className=' h-40 '>
-                <h5 className='font-semibold w-full'>Ilias</h5>
-                <p className='text-gray-500 mb-4'>August 2021</p>
-                <p className>This is the third time I stay at Lateef’s and every time it’s a wonderful experience. The flat is sparkling clean, very spacious and Lateef is so nice and helpful. 10/10 will always recommend.</p>
-            </div> */}
+    
         </div>
         </div>
         <Footer />
